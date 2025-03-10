ef8d499043bee4395b0cfd51e43e435c6a1bf4d6
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 19:42:57 +0300

    added functionality to the counter button

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
+//create a class for stateless widget to override the home page
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //call Robot counter class to start  the count down
     return MaterialApp(home: RobotCounter());
   }
 }
 
-//class to create a stateful widget
+//create stateful widget to create the counter
 class RobotCounter extends StatefulWidget {
+  //call the robot counter function to set the counter
   @override
   RobotCounterState createState() => RobotCounterState();
 }
 
-//class to set the widget counter
+// a class to set the counter
 class RobotCounterState extends State<RobotCounter> {
   int count = 0;
-  void setCounter() {
+  void countDown() {
     setState(() {
       count = count + 1;
     });
   }
 
+  //override the screen with the new counter
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My App counter',
+          style: TextStyle(fontSize: 20, color: Colors.red),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Robot dance moved: $count", style: TextStyle(fontSize: 20)),
-            SizedBox(height: 20),
+            Text('Number of clicks: $count', style: TextStyle(fontSize: 20)),
+            ElevatedButton(
+              onPressed: countDown,
+              child: Text(
+                '+',
+                style: TextStyle(fontSize: 30, color: Colors.red),
+              ),
+            ),
           ],
         ),
       ),
