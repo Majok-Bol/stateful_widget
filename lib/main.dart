143ef5d3214dd67ae9a567e3eee792cff8f0786f
Majok-Bol <bolmajok001@gmail.com>
2025-03-10 18:23:36 +0300

    created counter function class

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: RobotCounter());
+  }
+}
+
+//class to create a stateful widget
+class RobotCounter extends StatefulWidget {
+  @override
+  RobotCounterState createState() => RobotCounterState();
+}
+
+//class to set the widget counter
+class RobotCounterState extends State<RobotCounter> {
+  int count = 0;
+  void setCounter() {
+    setState(() {
+      count = count + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Robot dance moved: $count", style: TextStyle(fontSize: 20)),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
